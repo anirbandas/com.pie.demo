96c1bdd81ce35de653d30b3b08171848e496563e
anirbandas <anirbandas_ju@yahoo.co.in>
2013-09-30 18:28:07 +0530

    version 1 changes

--- src/com/pie/demo/pie.java
+++ src/com/pie/demo/pie.java
@@ -8,6 +8,14 @@ public class pie {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pie products..");
+		
+		System.out.println("version 1 pie products : ");
+		System.out.println(getOldPieProducts());
+	}
+	
+	public static String getOldPieProducts()
+	{
+		return "Pie old 1, Pie old 2";
 	}
 
 }
